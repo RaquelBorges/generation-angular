bec2a3e1f26d6cfd9ed72661140f526f29d90c79
Raquel Borges <raquel.olborges@gmail.com>
2021-03-18 11:29:10 -0300

    Definição das rotas login e cadastrar

--- Blog Pessoal/blogPessoal/src/app/app-routing.module.ts
+++ Blog Pessoal/blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login", component: LoginComponent},
+  {path:"cadastrar", component: CadastrarComponent},
+  {path:"", redirectTo: "login", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
